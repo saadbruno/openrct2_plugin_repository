618996a71916fa13ff5fe961a4067c36795f3b56
Bruno Saad Marques <saadbruno@gmail.com>
2020-05-06 01:48:06 -0300

    adds google analytics

--- code/views/home/partials/bottom.php
+++ code/views/home/partials/bottom.php
@@ -3,3 +3,13 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 <script src="/public/js/orct2p.js?v=<?= $config['version'] ?>"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11515286-12"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-11515286-12');
+</script>
