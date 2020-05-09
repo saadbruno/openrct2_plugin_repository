7a83790451cecf020d2074c4d43612c1a1e5e3cd
Bruno Saad Marques <saadbruno@gmail.com>
2020-05-09 19:50:03 -0300

    moving submit logic to a function (so we can reuse it)

--- code/lib/db/submit.php
+++ code/lib/db/submit.php
@@ -1,34 +1,38 @@
 <?php
 
-// all the logic for submitting new plugins
+function savePlugin($githubUrl, $skipDate = false)
+{
+  // all the logic for submitting new plugins
 
-// gets submitted url (sanitize, cause why not?)
-$url['raw'] = filter_input(INPUT_POST, 'githubUrl', FILTER_SANITIZE_URL);
+  global $pdo;
 
-// parsed the URL to get the path
-$url['parsed'] = parse_url($url['raw']);
+  // gets submitted url
+  $url['raw'] = $githubUrl;
 
-// explodes every slash into an array owner will be [1] and name will be [2]
-$url['repoData'] = explode("/", $url['parsed']['path']);
+  // parsed the URL to get the path
+  $url['parsed'] = parse_url($url['raw']);
 
-// removes ".git" from name, in case user submitted the .git URL
-$url['repoData'][2] = str_replace(".git", "", $url['repoData'][2]);
+  // explodes every slash into an array owner will be [1] and name will be [2]
+  $url['repoData'] = explode("/", $url['parsed']['path']);
 
-// VALIDATION
-// if it's not https, the host is not github.com, either of the repo datas are empty, throw an error
-if ($url['parsed']['scheme'] != 'https' || $url['parsed']['host'] != 'github.com' || empty($url['repoData'][1]) || empty($url['repoData'][2]) ) {
-  header('Content-Type: application/json; charset=UTF-8');
-  $result=array();
-  $result['status'] = 'error';
-  $result['message'] = 'URL is not valid';
-  die(json_encode($result));
-}
+  // removes ".git" from name, in case user submitted the .git URL
+  $url['repoData'][2] = str_replace(".git", "", $url['repoData'][2]);
 
-// puts array into variables so we can use them below:
-$repoOwner = $url['repoData'][1];
-$repoName = $url['repoData'][2];
+  // VALIDATION
+  // if it's not https, the host is not github.com, either of the repo datas are empty, throw an error
+  if ($url['parsed']['scheme'] != 'https' || $url['parsed']['host'] != 'github.com' || empty($url['repoData'][1]) || empty($url['repoData'][2])) {
+    header('Content-Type: application/json; charset=UTF-8');
+    $result = array();
+    $result['status'] = 'error';
+    $result['message'] = 'URL is not valid';
+    die(json_encode($result));
+  }
 
-$gitQuery = <<<GRAPHQL
+  // puts array into variables so we can use them below:
+  $repoOwner = $url['repoData'][1];
+  $repoName = $url['repoData'][2];
+
+  $gitQuery = <<<GRAPHQL
 query {
     repository(owner: "$repoOwner", name: "$repoName") {
       id
@@ -87,40 +91,40 @@ query {
   }
 
 GRAPHQL;
-$result = graphql_query('https://api.github.com/graphql', $gitQuery, [], $_ENV['GITHUB_TOKEN']);
+  $result = graphql_query('https://api.github.com/graphql', $gitQuery, [], $_ENV['GITHUB_TOKEN']);
 
-debug($result, 'Raw GitHub API result');
+  debug($result, 'Raw GitHub API result');
 
-// converts the timestamp
-$updatedAt =  date("U",strtotime($result['data']['repository']['updatedAt']));
-$submittedAt = time();
+  // converts the timestamp
+  $updatedAt =  date("U", strtotime($result['data']['repository']['updatedAt']));
+  $submittedAt = time();
 
-// converts OG image to boolean
-if ($result['data']['repository']['usesCustomOpenGraphImage'] == 1) {
-  $usesOGImg = 1;
-} else {
-  $usesOGImg = 0;
-}
+  // converts OG image to boolean
+  if ($result['data']['repository']['usesCustomOpenGraphImage'] == 1) {
+    $usesOGImg = 1;
+  } else {
+    $usesOGImg = 0;
+  }
 
-// deals with License inconsistencies
-$license = $result['data']['repository']['licenseInfo']['nickname'];
-// if there's no nickname, use the spdxId
-if(empty($license)) {
-  $license = $result['data']['repository']['licenseInfo']['spdxId'];
-}
-// if the spdxId is "NOASSERTION", then use the actual name
-if($license == 'NOASSERTION') {
-  $license = $result['data']['repository']['licenseInfo']['name'];
-} 
+  // deals with License inconsistencies
+  $license = $result['data']['repository']['licenseInfo']['nickname'];
+  // if there's no nickname, use the spdxId
+  if (empty($license)) {
+    $license = $result['data']['repository']['licenseInfo']['spdxId'];
+  }
+  // if the spdxId is "NOASSERTION", then use the actual name
+  if ($license == 'NOASSERTION') {
+    $license = $result['data']['repository']['licenseInfo']['name'];
+  }
 
-// Appends all the 4 readme checks. Since most likely only 1 of them will have any text, we can just append them all. Easier than doing a bunch of if/ elses
-$readme = $result['data']['repository']['readme1']['text'];
-$readme .= $result['data']['repository']['readme2']['text'];
-$readme .= $result['data']['repository']['readme3']['text'];
-$readme .= $result['data']['repository']['readme4']['text'];
+  // Appends all the 4 readme checks. Since most likely only 1 of them will have any text, we can just append them all. Easier than doing a bunch of if/ elses
+  $readme = $result['data']['repository']['readme1']['text'];
+  $readme .= $result['data']['repository']['readme2']['text'];
+  $readme .= $result['data']['repository']['readme3']['text'];
+  $readme .= $result['data']['repository']['readme4']['text'];
 
-// PLUGINS db insert
-$sql = "INSERT INTO `plugins`
+  // PLUGINS db insert
+  $sql = "INSERT INTO `plugins`
         (`id`, `name`, `url`, `description`, `submittedAt`, `updatedAt`, `usesCustomOpenGraphImage`, `thumbnail`, `stargazers`, `owner`, `readme`, `licenseName`, `licenseUrl`) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
@@ -137,46 +141,46 @@ $sql = "INSERT INTO `plugins`
         `licenseName` = ?,
         `licenseUrl` = ?
         ;";
-try {
+  try {
     $pdo->prepare($sql)->execute([
-        $result['data']['repository']['id'],
-        $result['data']['repository']['name'],
-        $result['data']['repository']['url'],
-        $result['data']['repository']['description'],
-        $submittedAt,
-        $updatedAt,
-        $usesOGImg,
-        $result['data']['repository']['openGraphImageUrl'],
-        $result['data']['repository']['stargazers']['totalCount'],
-        $result['data']['repository']['owner']['id'],
-        $readme,
-        $license,
-        $result['data']['repository']['licenseInfo']['url'],
-        $result['data']['repository']['name'],
-        $result['data']['repository']['url'],
-        $result['data']['repository']['description'],
-        $submittedAt,
-        $updatedAt,
-        $usesOGImg,
-        $result['data']['repository']['openGraphImageUrl'],
-        $result['data']['repository']['stargazers']['totalCount'],
-        $result['data']['repository']['owner']['id'],
-        $readme,
-        $license,
-        $result['data']['repository']['licenseInfo']['url']
+      $result['data']['repository']['id'],
+      $result['data']['repository']['name'],
+      $result['data']['repository']['url'],
+      $result['data']['repository']['description'],
+      $submittedAt,
+      $updatedAt,
+      $usesOGImg,
+      $result['data']['repository']['openGraphImageUrl'],
+      $result['data']['repository']['stargazers']['totalCount'],
+      $result['data']['repository']['owner']['id'],
+      $readme,
+      $license,
+      $result['data']['repository']['licenseInfo']['url'],
+      $result['data']['repository']['name'],
+      $result['data']['repository']['url'],
+      $result['data']['repository']['description'],
+      $submittedAt,
+      $updatedAt,
+      $usesOGImg,
+      $result['data']['repository']['openGraphImageUrl'],
+      $result['data']['repository']['stargazers']['totalCount'],
+      $result['data']['repository']['owner']['id'],
+      $readme,
+      $license,
+      $result['data']['repository']['licenseInfo']['url']
     ]);
-} catch (Exception $e) {
+  } catch (Exception $e) {
     header('HTTP/1.1 500 Internal Server Error');
     header('Content-Type: application/json; charset=UTF-8');
-    $result=array();
+    $result = array();
     $result['status'] = 'error';
     $result['message'] = 'Error when adding plugin';
     die(json_encode($result));
-}
+  }
 
 
-// USERS db insert
-$sql = "INSERT INTO `users`
+  // USERS db insert
+  $sql = "INSERT INTO `users`
         (`id`, `username`, `avatarUrl`, `url`) 
         VALUES (?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
@@ -184,64 +188,65 @@ $sql = "INSERT INTO `users`
         `avatarUrl` = ?, 
         `url` = ?
         ;";
-try {
+  try {
     $pdo->prepare($sql)->execute([
-        $result['data']['repository']['owner']['id'],
-        $result['data']['repository']['owner']['login'],
-        $result['data']['repository']['owner']['avatarUrl'],
-        $result['data']['repository']['owner']['url'],
-        $result['data']['repository']['owner']['login'],
-        $result['data']['repository']['owner']['avatarUrl'],
-        $result['data']['repository']['owner']['url']
+      $result['data']['repository']['owner']['id'],
+      $result['data']['repository']['owner']['login'],
+      $result['data']['repository']['owner']['avatarUrl'],
+      $result['data']['repository']['owner']['url'],
+      $result['data']['repository']['owner']['login'],
+      $result['data']['repository']['owner']['avatarUrl'],
+      $result['data']['repository']['owner']['url']
     ]);
-} catch (Exception $e) {
-  header('HTTP/1.1 500 Internal Server Error');
-  header('Content-Type: application/json; charset=UTF-8');
-  $result=array();
-  $result['status'] = 'error';
-  $result['message'] = 'Error when adding user';
-  die(json_encode($result));
-}
+  } catch (Exception $e) {
+    header('HTTP/1.1 500 Internal Server Error');
+    header('Content-Type: application/json; charset=UTF-8');
+    $result = array();
+    $result['status'] = 'error';
+    $result['message'] = 'Error when adding user';
+    die(json_encode($result));
+  }
 
 
-// TAGS db insert
-foreach ($result['data']['repository']['repositoryTopics']['edges'] as $key => $topic) {
-  $sql = "INSERT INTO `tags`
+  // TAGS db insert
+  foreach ($result['data']['repository']['repositoryTopics']['edges'] as $key => $topic) {
+    $sql = "INSERT INTO `tags`
           (`plugin_id`, `tag`) 
           VALUES (?, ?)
           ON DUPLICATE KEY UPDATE
           `plugin_id` = ?,
           `tag` = ?
           ;";
-  try {
+    try {
       $pdo->prepare($sql)->execute([
-          $result['data']['repository']['id'],
-          $topic['node']['topic']['name'],
-          $result['data']['repository']['id'],
-          $topic['node']['topic']['name']
+        $result['data']['repository']['id'],
+        $topic['node']['topic']['name'],
+        $result['data']['repository']['id'],
+        $topic['node']['topic']['name']
       ]);
-  } catch (Exception $e) {
-    header('HTTP/1.1 500 Internal Server Error');
-    header('Content-Type: application/json; charset=UTF-8');
-    $result=array();
-    $result['status'] = 'error';
-    $result['message'] = 'Error when adding tags';
-    die(json_encode($result));
+    } catch (Exception $e) {
+      header('HTTP/1.1 500 Internal Server Error');
+      header('Content-Type: application/json; charset=UTF-8');
+      $result = array();
+      $result['status'] = 'error';
+      $result['message'] = 'Error when adding tags';
+      die(json_encode($result));
+    }
   }
+
+  // if we got to this point, everything went well. Send message back saying so.
+
+  // builds redirect url
+  $redirect = "/plugin/" . $result['data']['repository']['id'] . "/" . urlencode($result['data']['repository']['name']);
+
+  header('Content-Type: application/json; charset=UTF-8');
+  $result = array();
+  $result['status'] = 'success';
+  $result['message'] = 'success';
+  $result['redirect'] = $redirect;
+  die(json_encode($result));
+
+  // debugging code
+  debug($url, "URL Debugging");
+  debug($result, "GitHub API response");
 }
-
-// if we got to this point, everything went well. Send message back saying so.
-
-// builds redirect url
-$redirect = "/plugin/" . $result['data']['repository']['id'] . "/" . urlencode($result['data']['repository']['name']);
-
-header('Content-Type: application/json; charset=UTF-8');
-$result=array();
-$result['status'] = 'success';
-$result['message'] = 'success';
-$result['redirect'] = $redirect;
-die(json_encode($result));
-
-// debugging code
-debug($url, "URL Debugging");
-debug($result, "GitHub API response");
