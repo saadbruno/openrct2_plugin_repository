55ab6c2fd2834146d31c275a8acec81ffe4b8fe0
Bruno Saad Marques <saadbruno@gmail.com>
2020-05-09 23:39:37 -0300

    finished writing script to update database

--- code/bin/update_plugins.php
+++ code/bin/update_plugins.php
@@ -1,6 +1,6 @@
 <?php
 // this script updates all plugins in the database with the latest information from github, without updating the "submitted time", so we don't change the home page order
-// usage: php update_plugins.php GitHub API Token <optional: gihtub link>
+// usage: php update_plugins.php <optional: gihtub link>
 
 // exclusive to command line
 if (php_sapi_name() != 'cli') {
@@ -8,24 +8,15 @@ if (php_sapi_name() != 'cli') {
     exit;
 }
 
-if(!$argv[1]) {
-    die('you need to provide a github API token');
-}
+require_once "../lib/functions.php";
+require_once "../lib/db/db_conn.php";
+require_once "../lib/db/getPlugins.php";
+require_once "../lib/db/submit.php";
 
-$token = $argv[1];
-putenv("GITHUB_TOKEN=$token");
 
-require_once "../code/lib/functions.php";
-require_once "../code/lib/db/db_conn.php";
-require_once "../code/lib/db/getPlugins.php";
-require_once "../code/lib/db/submit.php";
+if ($argv[1]) {
 
-print_r($_ENV);
-
-exit();
-if ($argv[2]) {
-
-    $githubUrl = $argv[2];
+    $githubUrl = $argv[1];
     savePlugin($githubUrl, true);
     
 } else {
@@ -37,13 +28,14 @@ if ($argv[2]) {
     $query .= "ON `plugins`.`owner` = `users`.`id`;";
 
     // get plugins
-    $stmt_plugins = $pdo->prepare($query)->execute();
+    $stmt_plugins = $pdo->prepare($query); 
+    $stmt_plugins->execute();
+    
 
-    // loops each plugin
     while ($row_plugins = $stmt_plugins->fetch()) {
 
         // builds the github URL
-        $githubUrl = "https://github.com/" . $row_plugins['name'] . "/" . $row_plugins['username'];
+        $githubUrl = "https://github.com/" . $row_plugins['username'] . "/" . $row_plugins['name'];
         savePlugin($githubUrl, true);
     }
 }
